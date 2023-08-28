4f4cd52bb07cc2ddeae6a0da588630505cf5046b
useHWSeo <gmldnjseocnd@naver.com>
2023-08-28 22:24:20 +0900

    feat: MUI 컴포넌트 추가 - MUI 컴포넌트 추가 및 models로 해당 컴포넌트들 배열화

---
+++ src/mui-models/models.tsx
@@ -0,0 +1,41 @@
+import AutoComplete from "../mui_component/AutoComplete";
+import BasicButtonGroup from "../mui_component/BasicButtonGroup";
+import BasicPagination from "../mui_component/BasicPagination";
+import BasicRating from "../mui_component/BasicRating";
+import BasicSelect from "../mui_component/BasicSelect";
+import BasicSwitches from "../mui_component/BasicSwitches";
+import BasicTabs from "../mui_component/BasicTabs";
+import BasicTooltip from "../mui_component/BasicTooltip";
+import BreadCrumbs from "../mui_component/BreadCrumbs";
+import Checkboxes from "../mui_component/CheckBoxes";
+import CircularIndeterminate from "../mui_component/CircularIndeterminate";
+import ContinuousSlider from "../mui_component/ContinuousSlider";
+import FloatingActionButtons from "../mui_component/FloatingActionButtons";
+import RadioButtonsGroup from "../mui_component/RadioButtonsGroup";
+import SimpleBadge from "../mui_component/SimpleBadge";
+import SimpleDialogDemo from "../mui_component/SimpleDialogDemo";
+
+const muiModels = [
+  { component: <AutoComplete />, name: "AutoComplete" },
+  { component: <BasicButtonGroup />, name: "ButtonGroup" },
+  { component: <BasicPagination />, name: "Pagination" },
+  { component: <BasicRating />, name: "Rating" },
+  { component: <BasicSelect />, name: "Select" },
+  { component: <BasicSwitches />, name: "Switch" },
+  { component: <BasicTabs />, name: "Tabs" },
+  { component: <BasicTooltip />, name: "Tooltip" },
+  { component: <BreadCrumbs />, name: "BreadCrumbs" },
+  { component: <Checkboxes />, name: "Checkbox" },
+  { component: <CircularIndeterminate />, name: "CircleProgress" },
+  { component: <ContinuousSlider />, name: "Slider" },
+  { component: <FloatingActionButtons />, name: "FloatingActionButtons" },
+  { component: <RadioButtonsGroup />, name: "RadioButtonsGroup" },
+  { component: <SimpleBadge />, name: "Badge" },
+  { component: <SimpleDialogDemo />, name: "Dialog" },
+];
+
+export const mutipleChoice = (answer: string) => {
+  const choiceList = [answer];
+  const randomNum = Math.floor(Math.random() * muiModels.length);
+  console.log(randomNum);
+};
